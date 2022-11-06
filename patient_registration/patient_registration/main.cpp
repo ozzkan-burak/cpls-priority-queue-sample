c055d65623d0db0cbd027017bafc0f0febc4e144
burak.ozkan <burak.ozkan@flo.com.tr>
2022-11-06 23:41:37 +0300

    project-init

---
+++ patient_registration/patient_registration/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  patient_registration
+//
+//  Created by Burak ÖZKAN on 6.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
